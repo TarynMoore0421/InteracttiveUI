f82a427d3be3f53b35997326e32b8031c1a3ee18
Taryn <tamoore@gmail.com>
2025-06-04 14:18:04 -0500

    Initial Commit

---
+++ InteracttiveUI/InteracttiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteracttiveUIApp.swift
+//  InteracttiveUI
+//
+//  Created by Scholar on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct InteracttiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
